2f13bb9159172c0b326528ee2e88861d9b5210fa
andikk <andikk@mail.ru>
2019-06-18 15:09:41 +0400

    Поправил заполнения поля цена

--- js/main.js
+++ js/main.js
@@ -132,16 +132,16 @@ typeSelect.addEventListener('change', function () {
 
   switch (typeSelect.value) {
     case 'bungalo':
-      priceInput.value = '0';
+      priceInput.placeholder = '0';
       break;
     case 'flat':
-      priceInput.value = '1000';
+      priceInput.placeholder = '1000';
       break;
     case 'house':
-      priceInput.value = '5000';
+      priceInput.placeholder = '5000';
       break;
     case 'palace':
-      priceInput.value = '10000';
+      priceInput.placeholder = '10000';
       break;
   }
 });
