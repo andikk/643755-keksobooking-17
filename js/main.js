92e1a7895c7b3c7a9fecb3bb40f95b858807dfbb
andikk <andikk@mail.ru>
2019-06-20 18:34:40 +0400

    Вернул случайно удалённый код

--- js/main.js
+++ js/main.js
@@ -176,3 +176,38 @@ mapPinMain.addEventListener('mousedown', function (evt) {
   document.addEventListener('mouseup', onMouseUp);
 });
 
+var typeSelect = document.querySelector('#type');
+typeSelect.addEventListener('change', function () {
+  var priceInput = document.querySelector('#price');
+
+  switch (typeSelect.value) {
+    case 'bungalo':
+      priceInput.placeholder = '0';
+      break;
+    case 'flat':
+      priceInput.placeholder = '1000';
+      break;
+    case 'house':
+      priceInput.placeholder = '5000';
+      break;
+    case 'palace':
+      priceInput.placeholder = '10000';
+      break;
+  }
+});
+
+var timeInSelect = document.querySelector('#timein');
+var timeOutSelect = document.querySelector('#timeout');
+
+
+var changeSelectValue = function (selectedValue, selectForChange) {
+  selectForChange.value = selectedValue;
+};
+
+timeInSelect.addEventListener('change', function () {
+  changeSelectValue(timeInSelect.value, timeOutSelect);
+});
+
+timeOutSelect.addEventListener('change', function () {
+  changeSelectValue(timeOutSelect.value, timeInSelect);
+});
